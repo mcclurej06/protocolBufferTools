2ec81a8468605dfd7774b0346549c6cbf24cdca2
James McClure <mcclurej06@gmail.com>
2013-11-10 17:14:10 -0600

    cleanup for a little better readability

--- src/main/java/org/yogurt/protobufftools/ReflectiveEncoder.java
+++ src/main/java/org/yogurt/protobufftools/ReflectiveEncoder.java
@@ -1,21 +1,23 @@
 package org.yogurt.protobufftools;
 
+import org.reflections.Reflections;
+import org.reflections.scanners.FieldAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Set;
 
-public class ReflectiveEncoder implements IMessageEncoder{
+public class ReflectiveEncoder implements IMessageEncoder {
     public byte[] encode(Object o) throws Exception {
-        Class<?> buffer = o.getClass().getAnnotation(ProtoBufferData.class).protoBuffer();
-        Object builder = buffer.getMethod("newBuilder").invoke(null);
+        Object builder = getProtoBufferClass(o).getMethod("newBuilder").invoke(null);
 
-        for (Field f : o.getClass().getDeclaredFields()) {
+        for (Field f : getBufferFields(o)) {
             ProtoBufferField annotation = f.getAnnotation(ProtoBufferField.class);
-            if (annotation == null) {
-                continue;
-            }
 
-            Method setter = builder.getClass().getDeclaredMethod("set" + capitalize(annotation.fieldName()), f.getType());
-            Method getter = o.getClass().getMethod("get" + capitalize(f.getName()));
+            Method setter = getSetter(builder, annotation.fieldName(), f.getType());
+            Method getter = getGetter(o, f.getName());
             setter.invoke(builder, getter.invoke(o));
         }
 
@@ -30,24 +32,35 @@ public class ReflectiveEncoder implements IMessageEncoder{
 
         Object o = Class.forName(message.getMessageType()).newInstance();
 
-        Class<?> buffer = o.getClass().getAnnotation(ProtoBufferData.class).protoBuffer();
-        Object personProto = buffer.getMethod("parseFrom", byte[].class).invoke(null, message.getPayload());
+        Class<?> bufferClass = getProtoBufferClass(o);
+        Object personProto = bufferClass.getMethod("parseFrom", byte[].class).invoke(null, message.getPayload());
 
-
-        for (Field field : o.getClass().getDeclaredFields()) {
+        for (Field field : getBufferFields(o)) {
             ProtoBufferField annotation = field.getAnnotation(ProtoBufferField.class);
-            if (annotation == null) {
-                continue;
-            }
 
-            Method getter = personProto.getClass().getDeclaredMethod("get" + capitalize(annotation.fieldName()));
+            Method getter = getGetter(personProto, annotation.fieldName());
+            Method setter = getSetter(o, field.getName(), field.getType());
+
             Object data = getter.invoke(personProto);
-            Method setter = o.getClass().getMethod("set" + capitalize(field.getName()), field.getType());
             setter.invoke(o, data);
         }
 
-
         return o;
+    }
+
+    private Class<?> getProtoBufferClass(Object o) {
+        return o.getClass().getAnnotation(ProtoBufferData.class).protoBuffer();
+    }
+
+    private Set<Field> getBufferFields(Object o) {
+        return new Reflections(new ConfigurationBuilder().addUrls(ClasspathHelper.forClass(o.getClass())).setScanners(new FieldAnnotationsScanner())).getFieldsAnnotatedWith(ProtoBufferField.class);
+    }
+
+    private Method getSetter(Object o, String field, Class<?> type) throws NoSuchMethodException {
+        return o.getClass().getMethod("set" + capitalize(field), type);
+    }
+    private Method getGetter(Object o, String field) throws NoSuchMethodException {
+        return o.getClass().getMethod("get" + capitalize(field), (Class<?>[]) null);
     }
 
     private String capitalize(String line) {
